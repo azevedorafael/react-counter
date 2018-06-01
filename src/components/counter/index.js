e444faa6a2c0e880abc95a21c8dd579231f5d0a7
Rafael da S.P. de Azevedo <rafaelsilva.fatec@gmail.com>
2018-05-31 23:07:24 -0300

    Add multiple counter

--- src/components/counter/index.js
+++ src/components/counter/index.js
@@ -1,12 +1,16 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
+import { relative } from 'upath';
 
-const Counter = ({ counter, decrement, increment }) => (
-    <div>
+const Counter = ({ counter, decrement, increment, removeCounter }) => (
+    <div style={{ position: 'relative', border: '1px solid #000', padding: 5 }}>
         <h1>{counter}</h1>
         <button onClick={decrement}>-</button>
         <button onClick={increment}>+</button>
+        <button style={{ fontSize: 7, position: 'absolute', right: 5, top: 5 }} onClick={removeCounter}>
+            &times;
+        </button>
     </div>
 )
 
-export default(Counter)
+export default (Counter)
